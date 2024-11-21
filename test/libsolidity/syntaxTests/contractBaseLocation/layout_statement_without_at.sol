666b499bb579468d2759d969d1bb550f47f0df8f
Matheus Aguiar <matheus.pit@gmail.com>
2024-11-21 10:57:32 -0300

    fixup! update test

--- test/libsolidity/syntaxTests/contractBaseLocation/layout_statement_without_at.sol
+++ test/libsolidity/syntaxTests/contractBaseLocation/layout_statement_without_at.sol
@@ -1,4 +1,4 @@
 contract C layout { }
 // ----
-// ParserError 1994: (18-19): 'layout' should be followed by 'at <storage-base-expression>'.
+// ParserError 1994: (18-19): Expected 'at' but got '{'
 // ParserError 6933: (20-21): Expected primary expression.
